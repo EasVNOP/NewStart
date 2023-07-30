6a05b7aedca0277352dee6ccd910345c03b8e5da
EasVNOP <140871813+EasVNOP@users.noreply.github.com>
2023-07-30 02:01:47 +0000

    modified:   shells/install_panel/install.sh

--- shells/install_panel/install.sh
+++ shells/install_panel/install.sh
@@ -1,1 +1,1 @@
-curl -sSL https://resource.fit2cloud.com/1panel/package/quick_start.sh -o quick_start.sh && bash quick_start.sh
+curl -sSL https://resource.fit2cloud.com/1panel/package/quick_start.sh -o quick_start.sh && bash quick_start.sh -/workspaces/NewStart/applications/panel
